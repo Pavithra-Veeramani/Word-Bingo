46d02910b3e35e1c20f471be7300d52215d9dcaf
Pavithra-Veeramani <pavithra_veeramani@yahoo.com>
2022-04-18 15:34:46 +0000

    Use DOM instead of HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,33 @@
 
 var challengeWord = challengeWords[Math.floor(Math.random()*challengeWords.length)];
-
 let challengeWordsArray = Array.from(challengeWord);
-
+const numOfTextInput = 5;
 console.log(challengeWord);
+
+function onLoad(){
+    let textboxdiv = document.getElementsByClassName("text-box-row");
+    for(let i=0; i<textboxdiv.length; i++) {
+        for(let j=0; j < numOfTextInput; j++) {
+            let elText = document.createElement("input");
+            elText.className = "text-box";
+            elText.maxLength = 1;
+            elText.addEventListener("blur", toUpper(elText));
+            textboxdiv[i].appendChild(elText);
+        }
+        let elBtn = document.createElement("button");
+        elBtn.className = "word-check";
+        elBtn.innerHTML = "Check";
+        elBtn.addEventListener("click", checkWord);
+        textboxdiv[i].appendChild(elBtn);
+    }
+} 
 
 function toUpper(textbox){
     textbox.value = textbox.value.toUpperCase();
 }
 
-function checkWord(checkButton) {
-    let parentDiv = checkButton.parentNode;
+function checkWord(event) {
+    let parentDiv = event.target.parentNode;
     let childArr =  parentDiv.children;
 
     for(let i=0; i < childArr.length-1; i++) {
